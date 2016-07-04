a8087726ae0b59dc7d286c6872c49275574c04ac
maunilswadas <swadasmaunil@gmail.com>
2016-07-04 23:17:44 +0530

    Add: scene added,Update:server file updated

--- Assets/pythonServer.cs
+++ Assets/pythonServer.cs
@@ -8,49 +8,43 @@ using System.Text;
 using System.Threading;
 
 namespace samplechat1{
-public class pythonServer : MonoBehaviour {
+	public class pythonServer : MonoBehaviour {
 
-	IPEndPoint ipep,sender;
-	UdpClient newsock;
-	byte[] data = new byte[1024];
-	public static string val = "N";
+		IPEndPoint ipep,sender;
+		UdpClient newsock;
+		byte[] data = new byte[1024];
+		public static string val = "N";
 
 		ThreadStart recivingThread;
 		Thread childRecive;
-	// Use this for initialization
-	void Start () {
-
+		// Use this for initialization
+		void Start () {
 			recivingThread = new ThreadStart(CallToChildThread);
 			childRecive = new Thread (recivingThread);
-
-
-		ipep = new IPEndPoint(IPAddress.Any, 9050);
-		newsock = new UdpClient(ipep);
-		sender = new IPEndPoint (IPAddress.Any, 0);
-		data = newsock.Receive(ref sender);
-     
-        string welcome = "Welcome to my test server";
-        data = Encoding.ASCII.GetBytes(welcome);
-        newsock.Send(data, data.Length, sender);
+			ipep = new IPEndPoint(IPAddress.Any, 9050);
+			newsock = new UdpClient(ipep);
+			sender = new IPEndPoint (IPAddress.Any, 0);
+			data = newsock.Receive(ref sender);
+	     
+	        string welcome = "Welcome to my test server";
+	        data = Encoding.ASCII.GetBytes(welcome);
+	        newsock.Send(data, data.Length, sender);
 
 			childRecive.Start();
-			if(val.Equals("quit")) childRecive.Abort ();
-
 		}
 	
 	
-	// Update is called once per frame
-	void Update () {
-		   // data = newsock.Receive(ref sender);
-			//	val = Encoding.ASCII.GetString(data);
-	}
-	
+		// Update is called once per frame
+		void Update () {
+			   
+		}
+
 		public void CallToChildThread(){
 			while (!val.Equals("quit")) {
 				data = newsock.Receive(ref sender);
 				val = Encoding.ASCII.GetString(data);
 			}
+			if(val.Equals("quit")) childRecive.Abort ();
 		}
-
-}
+	}
 }
